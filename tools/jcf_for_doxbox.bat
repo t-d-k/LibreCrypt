434b6b1009829186ff46225b0229d21e3dd907e1
tdk <github.tdk@squte.com>
2014-09-09 10:31:28 +0100

    rename files

--- tools/jcf_for_doxbox.bat
+++ tools/jcf_for_doxbox.bat
@@ -1,2 +1,5 @@
-"C:\Apps\jcf\jcf.exe" -clarify -backup -y -config="P:\Projects\Delphi\doxbox\tools\JCF2Settings_for_doxbox.cfg" -F %1
+
+rem echo "%~n1.log" > out.log
+
+"C:\Apps\jcf\jcf.exe" -clarify -backup -y -config="P:\Projects\Delphi\doxbox\tools\JcfSettings_for_doxbox.cfg" -F %1 > "P:\Projects\Delphi\doxbox\tools\jcf_logs\%~n1.log"
 
