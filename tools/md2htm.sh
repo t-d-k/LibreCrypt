fc76812fee7039e3544a3d39eb6c8b061d678f25
tdk <github.tdk@squte.com>
2014-09-09 10:03:06 +0100

    autoformat tool

---
+++ tools/md2htm.sh
@@ -0,0 +1,17 @@
+while [ $# != 0 ]
+do
+	name=$1
+	shift
+	
+	# echo ${name}| sed 's/\.md/\.htm/'
+	html="`echo $name | sed 's/\.md/\.htm/'`"
+	# $html="echo ${name} | sed 's/\.md/\.htm/'"
+	echo $html
+	
+	perl ../tools/Markdown_1.0.1/Markdown.pl --html4tags $name > $html  
+
+done
+
+
+
+
