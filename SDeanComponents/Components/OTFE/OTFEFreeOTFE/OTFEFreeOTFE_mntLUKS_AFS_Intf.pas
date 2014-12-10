8b1fc6f2ddfeb88267d321700989a9f1cbb63ad4
tdk <github.tdk@squte.com>
2014-12-10 12:16:01 +0000

    refactor remove embedded includes and replace with functions or whole file

--- SDeanComponents/Components/OTFE/OTFEFreeOTFE/OTFEFreeOTFE_mntLUKS_AFS_Intf.pas
+++ SDeanComponents/Components/OTFE/OTFEFreeOTFE/OTFEFreeOTFE_mntLUKS_AFS_Intf.pas
@@ -1,45 +1,4 @@
-// ------------------------
-// "Private"
-function GetRandom(len: integer): string;
+{$Error obsolete - now in OTFEFreeOTFEBase_U}
 
-function RepeatedHash(
-                      hashKernelModeDeviceName: Ansistring;
-                      hashGUID: TGUID;
-                      input: Ansistring;
-                      var output: Ansistring
-                     ): boolean;
-function Process(
-                 stripeData: Ansistring;
-                 totalBlockCount: integer;
-                 blockLength: integer;
-                 hashKernelModeDeviceName: string;
-                 hashGUID: TGUID;
-                 var output: Ansistring
-                ): boolean;
-
-function GetBlock(stripeData: Ansistring; totalBlockCount: integer; blockLength: integer; blockNumber: integer; var output: Ansistring): boolean;
-
-
-// ------------------------
-// "Public"
-
-function AFSplit(
-                 input: AnsiString;
-                 n: integer;
-                 hashKernelModeDeviceName: string;
-                 hashGUID: TGUID;
-                 var output: AnsiString
-                ): boolean;
-
-function AFMerge(
-                 input: AnsiString;
-                 n: integer;
-                 hashKernelModeDeviceName: string;
-                 hashGUID: TGUID;
-                 var output: AnsiString
-                ): boolean;
-
-
-// ------------------------
 
 
