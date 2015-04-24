7b288d6a162913875fd059e2c1f85f8b5aeae7d2
tdk <github.tdk@squte.com>
2015-04-24 19:03:23 +0100

    refactor - extract common new password code into frame

---
+++ src/PC/gui/common/fmeNewPassword.dfm
@@ -0,0 +1,120 @@
+inherited frmeNewPassword: TfrmeNewPassword
+  Width = 511
+  Height = 292
+  Constraints.MinHeight = 223
+  Constraints.MinWidth = 223
+  ExplicitWidth = 511
+  ExplicitHeight = 292
+  DesignSize = (
+    511
+    292)
+  object lblInstructPassword: TLabel
+    Left = 0
+    Top = 0
+    Width = 511
+    Height = 78
+    Align = alTop
+    Caption = 
+      'Please enter the keyphrase to be used for securing your containe' +
+      'r'#10#13'Try to enter one character for each bit of the cypher keysize' +
+      '. For example for a 256 bit cypher enter a 256 character keyphra' +
+      'se.'#10#13'Note: If you forget your keyphrase, you can forget your dat' +
+      'a.'#10#13'Note: Newlines (blank lines) are significant. It is recommen' +
+      'ded that you do not press <ENTER> after typing in your keyphrase' +
+      ', as this will add an extra newline to the end of your keyphrase' +
+      '.'
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'MS Sans Serif'
+    Font.Style = []
+    ParentFont = False
+    WordWrap = True
+    ExplicitLeft = -207
+    ExplicitWidth = 514
+  end
+  object Label17: TLabel
+    Left = 3
+    Top = 99
+    Width = 111
+    Height = 13
+    Anchors = [akLeft, akTop, akRight]
+    Caption = '&Keyphrase:'
+    Constraints.MinWidth = 111
+  end
+  object Label3: TLabel
+    Left = 3
+    Top = 187
+    Width = 111
+    Height = 13
+    Anchors = [akLeft, akTop, akRight]
+    Caption = '&Confirm Keyphrase:'
+    Constraints.MinWidth = 111
+    FocusControl = preUserKeyConfirm
+  end
+  object lblStrength: TLabel
+    Left = 3
+    Top = 263
+    Width = 111
+    Height = 17
+    AutoSize = False
+    Caption = 'No Keyphrase'
+    Color = clBtnText
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clCaptionText
+    Font.Height = -11
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentColor = False
+    ParentFont = False
+    Transparent = True
+  end
+  object ProgressBar1: TProgressBar
+    Left = 120
+    Top = 263
+    Width = 375
+    Height = 17
+    Max = 128
+    MarqueeInterval = 1
+    BarColor = clAqua
+    BackgroundColor = clLime
+    TabOrder = 2
+    TabStop = True
+  end
+  object preUserKeyFirst: TOTFEFreeOTFE_PasswordRichEdit
+    Left = 120
+    Top = 96
+    Width = 375
+    Height = 73
+    Anchors = [akLeft, akTop, akRight]
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'MS Sans Serif'
+    Font.Style = []
+    Lines.Strings = (
+      '')
+    ParentFont = False
+    ScrollBars = ssBoth
+    TabOrder = 0
+    OnChange = preUserKeyFirstChange
+  end
+  object preUserKeyConfirm: TOTFEFreeOTFE_PasswordRichEdit
+    Left = 120
+    Top = 184
+    Width = 375
+    Height = 73
+    Anchors = [akLeft, akTop, akRight, akBottom]
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'MS Sans Serif'
+    Font.Style = []
+    Lines.Strings = (
+      '')
+    ParentFont = False
+    ScrollBars = ssBoth
+    TabOrder = 1
+    OnChange = preUserKeyConfirmChange
+  end
+end
---
+++ src/PC/gui/common/fmeNewPassword.pas
@@ -0,0 +1,246 @@
+unit fmeNewPassword;
+
+{
+copyright
+  (c) tdk
+  my code dual licenced under FreeOTFE licence and LGPL
+  code marked as 'sdean' is freeotfe licence only
+
+description
+  new kephrase, get KeyPhrase from KeyPhrase property
+  IsValid true iff match
+}
+
+interface
+
+uses
+  //delphi
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, SDUFrames, Vcl.StdCtrls, Vcl.ComCtrls,
+  //sdu
+  SDUGeneral,
+  //librecrypt
+  PasswordRichEdit, OTFEFreeOTFE_PasswordRichEdit, SDUComCtrls,
+  Vcl.Samples.Gauges;
+
+type
+  TfrmeNewPassword = class (TSDUFrame)
+    lblInstructPassword: TLabel;
+    Label17:         TLabel;
+    preUserKeyFirst: TOTFEFreeOTFE_PasswordRichEdit;
+    Label3:          TLabel;
+    preUserKeyConfirm: TOTFEFreeOTFE_PasswordRichEdit;
+    ProgressBar1:    TProgressBar;
+    lblStrength:     TLabel;
+
+    procedure preUserKeyConfirmChange(Sender: TObject);
+    procedure preUserKeyFirstChange(Sender: TObject);
+  private
+    FOnChange:     TNotifyEvent;
+    fKeyPhraseSet: Boolean; // has kephrase been set manually ('silent' mode)
+    procedure UpdateColours;
+    procedure UpdateStrength;
+
+  protected
+    procedure DoShow(); override;
+    procedure DoChange();
+  public
+    constructor Create(AOwner: TComponent); override;
+    procedure SetKeyPhrase(Value: TSDUBytes);
+    function IsPasswordValid: Boolean;
+    function GetKeyPhrase: TSDUBytes;
+  published
+    property OnChange: TNotifyEvent Read FOnChange Write FOnChange;
+  end;
+
+ //var
+ //  frmeNewPassword: TfrmeNewPassword;
+
+implementation
+
+{$R *.dfm}
+
+uses
+  SDUi18n, strutils,{CommCtrl,uxTheme,}
+
+  //librecrypt
+  OTFEFreeOTFEBase_U;
+
+const
+  G_ALLCHARS: TSysCharSet              = [#0..#127];
+  G_ALLBYTES: TSysCharSet              = [#0..#255];
+  G_ALPHASYMBOLCHARS: TSysCharSet      = [#33..#126];// all alphanumeric + nonspace specials
+  G_SYMBOLCHARS: TSysCharSet           = [#33..#47, #58..#64, #91..#96, #123..#126];
+  // all nonspace specials xcept numbers
+  G_WHITESPACEANDNONPRINT: TSysCharSet = [#0..#32, #127]; // all ascii controls + space
+  G_NONPRINT: TSysCharSet              = [#0..#8, #14..#31, #127]; // all ascii controls
+  G_WHITESPACE: TSysCharSet            = [#9..#13, ' ']; //  printable whitespace
+  G_ALPHANUM: TSysCharSet              = ['0'..'9', 'A'..'Z', 'a'..'z'];//  alphanumeric
+  G_ALPHANUM_APPOS: TSysCharSet        = ['0'..'9', 'A'..'Z', 'a'..'z', ''''];
+  //  alphanumeric  + appostrophe
+  G_ALPHA: TSysCharSet                 = ['A'..'Z', 'a'..'z'];//  alphabet
+  G_NUMCHARS: TSysCharSet              = ['0'..'9'];//  numeric
+  G_CAPITALS: TSysCharSet              = ['A'..'Z'];
+  G_VALID_FILENAME: TSysCharSet        = [#43..#46, #48..#57, #65..#90, #95, #97..#122];
+
+{ TfrmeNewPassword }
+
+constructor TfrmeNewPassword.Create(AOwner: TComponent);
+begin
+  inherited;
+  fKeyPhraseSet := False;
+end;
+
+procedure TfrmeNewPassword.DoChange;
+begin
+  if Assigned(fonchange) then
+    fonchange(self);
+end;
+
+procedure TfrmeNewPassword.DoShow;
+begin
+  inherited;
+  SDUTranslateComp(lblInstructPassword);
+
+
+  // tsPassword
+  preUserKeyFirst.Plaintext   := True;
+  // FreeOTFE volumes CAN have newlines in the user's password
+  preUserKeyFirst.WantReturns := True;
+  preUserKeyFirst.WordWrap    := True;
+
+  preUserKeyFirst.PasswordChar := GetFreeOTFEBase().PasswordChar;
+  preUserKeyFirst.WantReturns  := GetFreeOTFEBase().AllowNewlinesInPasswords;
+  preUserKeyFirst.WantTabs     := GetFreeOTFEBase().AllowTabsInPasswords;
+
+  preUserKeyConfirm.Plaintext   := True;
+  // FreeOTFE volumes CAN have newlines in the user's password
+  preUserKeyConfirm.WantReturns := True;
+  preUserKeyConfirm.WordWrap    := True;
+  // if manually set ('silent' mode) don;t clear
+  if not fKeyPhraseSet then begin
+    preUserKeyConfirm.Lines.Clear();
+    preUserKeyFirst.Lines.Clear();
+  end;
+
+  preUserKeyConfirm.PasswordChar := GetFreeOTFEBase().PasswordChar;
+  preUserKeyConfirm.WantReturns  := GetFreeOTFEBase().AllowNewlinesInPasswords;
+  preUserKeyConfirm.WantTabs     := GetFreeOTFEBase().AllowTabsInPasswords;
+end;
+
+function TfrmeNewPassword.IsPasswordValid(): Boolean;
+begin
+  Result := (preUserKeyFirst.Text = preUserKeyConfirm.Text) and (preUserKeyFirst.Text <> '');
+end;
+
+
+procedure TfrmeNewPassword.UpdateColours();
+var
+  aCol: TColor;
+const
+  // colours from system.UITypes
+  Lightblue  = TColor($E6D8AD);
+  Lightpink  = TColor($C1B6FF);
+  Lightgreen = TColor($90EE90);
+begin
+  // if password complete and same then green, if different then red. if same up to length entered in 2nd, then blue
+  if preUserKeyConfirm.Text = '' then
+    aCol := clWhite
+  else
+  if (preUserKeyFirst.Text = preUserKeyConfirm.Text) then
+    aCol := Lightgreen
+  else
+  if AnsiStartsStr(preUserKeyConfirm.Text, preUserKeyFirst.Text) then
+    aCol := Lightblue
+  else
+    aCol := Lightpink;
+
+  //too subtle - change background
+  //  preUserKeyFirst.Font.Color := aCol;
+  //   preUserKeyConfirm.Font.Color := aCol;
+  preUserKeyConfirm.Color := aCol;
+end;
+
+procedure TfrmeNewPassword.UpdateStrength();
+var
+  aBits: Double;
+  i:     Integer;
+  aCol:  TColor;
+  stren: String;
+  ch:    Char;
+begin
+  aBits := 0;
+  //very crude estimate of ntropy - helps shoulder surfers so add option to disable
+  for i := 1 to length(preUserKeyFirst.Text) - 1 do begin
+    ch := preUserKeyFirst.Text[i];
+    if (ch in G_ALPHA) or (ch in G_WHITESPACE) then
+      aBits := aBits + 1.1
+    else
+    if ch in G_NUMCHARS then
+      aBits := aBits + 1.5
+    else
+      aBits := aBits + 2;
+
+  end;
+  ProgressBar1.Position := trunc(aBits);
+
+  if preUserKeyFirst.Text = '' then begin
+    aCol  := clBlack;
+    stren := _('No keyphrase');
+  end else begin
+    if aBits < 56 then begin
+      aCol  := clRed;
+      stren := _('Keyphrase weak');
+    end else begin
+      if aBits < 128 then begin
+        aCol  := clBlue;
+        stren := _('Keyphrase OK');
+      end else begin
+        aCol  := clGreen;
+        stren := _('Keyphrase strong');
+      end;
+    end;
+  end;
+
+
+  //this doesnt work with theme
+  ProgressBar1.BarColor  := aCol;
+  //    ProgressBar1.BackgroundColor  := clYellow;
+  lblStrength.Font.color := aCol;
+  lblStrength.Caption    := stren;
+ {  SetWindowTheme(self.Handle, nil, nil);
+    ProgressBar1.Brush.Color:= clRed; // Set Background colour
+SendMessage (ProgressBar1.Handle, PBM_SETBARCOLOR, 0, clBlue); // Set bar colour   }
+end;
+
+procedure TfrmeNewPassword.preUserKeyConfirmChange(Sender: TObject);
+begin
+  inherited;
+  UpdateColours;
+  DoChange();
+end;
+
+procedure TfrmeNewPassword.preUserKeyFirstChange(Sender: TObject);
+begin
+  inherited;
+  UpdateColours;
+  UpdateStrength();
+  DoChange();
+end;
+
+function TfrmeNewPassword.GetKeyPhrase(): TSDUBytes;
+begin
+  { TODO 1 -otdk -cbug : handle non ascii user keys - at least warn user }
+  Result := SDUStringToSDUBytes(preUserKeyFirst.Text);
+end;
+
+procedure TfrmeNewPassword.SetKeyPhrase(Value: TSDUBytes);
+begin
+  { TODO 1 -otdk -cbug : handle non ascii user keys - at least warn user }
+  preUserKeyFirst.Text   := SDUBytesToString(Value);
+  preUserKeyConfirm.Text := preUserKeyFirst.Text;
+  fKeyPhraseSet          := True;
+
+end;
+
+end.
