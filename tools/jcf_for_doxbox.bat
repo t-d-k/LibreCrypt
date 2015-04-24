bfe5a23b8b6495fa6876566ecdc0279ae653bfe4
tdk <github.tdk@squte.com>
2015-04-24 18:44:36 +0100

    subst directory change

--- tools/jcf_for_doxbox.bat
+++ tools/jcf_for_doxbox.bat
@@ -1,5 +1,5 @@
 
 rem echo "%~n1.log" > out.log
 
-"C:\Apps\jcf\jcf.exe" -clarify -backup -y -config="P:\Projects\Delphi\doxbox\tools\JcfSettings_for_doxbox.cfg" -F %1 > "P:\Projects\Delphi\doxbox\tools\jcf_logs\%~n1.log"
+"C:\Apps\jcf\jcf.exe" -clarify -backup -y -config="P:\tools\JcfSettings_for_doxbox.cfg" -F %1 > "P:\tools\jcf_logs\%~n1.log"
 
